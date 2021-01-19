31e5e4ae42c872c9146c3bfa967531c8d3d92559
Damian Barabonkov <damianb@mit.edu>
2021-01-19 08:45:18 -0500

    API supports check if webauthn is enabled for user

--- webauthn.go
+++ webauthn.go
@@ -3,3 +3,8 @@ package gogs
 type WebauthnContainer struct {
 	WebauthnData string `json:"webauthn_data"`
 }
+
+func (c *Client) IsWebauthnEnabled() (*bool, error) {
+	var enabled bool
+	return &enabled, c.getParsedResponse("GET", "/webauthn/isenabled", nil, nil, &enabled)
+}
